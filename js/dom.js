a3bad15450e798ce9d7429317155dc88d08f1895
JhonyGestoru <jhonygestoru@gmail.com>
2023-03-01 08:21:31 -0500

    coger texto de un input con javascript

--- js/dom.js
+++ js/dom.js
@@ -1,17 +1,23 @@
 // DOM
-document.querySelector(".azul").style.
-    backgroundColor = "blue";
-document.querySelector("#texto").innerHTML = "Programacion web 2";
+// document.querySelector(".azul").style.
+//     backgroundColor = "blue";
+// document.querySelector("#texto").innerHTML = "Programacion web 2";
+//
+// const nodelist = document.querySelectorAll("p");
+//     nodelist[0].style.backgroundColor = "red";
+//
+//     let cont = document.querySelectorAll("p").length;
+//     document.write(cont);
+//     document.querySelector("#resp").innerHTML =cont;
+//
+//     const list = document.querySelectorAll(".example");
+//     for (i=0; i < list.length; i++){
+//         list[i].classList.toggle("prueba1");
+// }
 
-const nodelist = document.querySelectorAll("p");
-    nodelist[0].style.backgroundColor = "red";
-
-    let cont = document.querySelectorAll("p").length;
-    document.write(cont);
-    document.querySelector("#resp").innerHTML =cont;
-
-    const list = document.querySelectorAll(".example");
-    for (i=0; i < list.length; i++){
-        list[i].classList.toggle("prueba1");
+function enviarTexto() {
+    var inputElement = document.getElementById("textInput");
+    var texto = inputElement.value;
+    alert("El texto ingresado es: " + texto);
 }
 
